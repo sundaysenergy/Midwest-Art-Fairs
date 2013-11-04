c5df290827970667057b8178aa1c85161b00ba2a
Tim Lupfer <tim@sundaysenergy.com>
2013-11-04 16:28:18 -0600

    Move event javascript to its own file and geolocate by default

---
+++ js/event.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+  // Datepickers for start and end date
+  $('.input-group.start-date').datepicker({
+    autoclose: true
+  });
+  $('.input-group.end-date').datepicker({
+    autoclose: true
+  });
+
+  // Create gmaps.js object
+  var map = new GMaps({
+    div: '#map',
+    lat: 44.44107850000001,
+    lng: -92.14807874603275,
+    zoom: 5
+  });
+
+  // Set location to current location
+  GMaps.geolocate({
+    success: function(position) {
+      map.setCenter(position.coords.latitude, position.coords.longitude);
+    }
+  });
+
+  // Create a pin from the address
+  $('#find-location').on('click touch', function(e) {
+    e.preventDefault();
+    GMaps.geocode({
+      address: $('#address').val(),
+      callback: function(results, status) {
+        if (status == 'OK') {
+          var latlng = results[0].geometry.location;
+          map.setCenter(latlng.lat(), latlng.lng());
+          map.removeMarkers();
+          map.addMarker({
+            lat: latlng.lat(),
+            lng: latlng.lng(),
+            draggable: true,
+            dragend: function(e) {
+              var marker = map.markers.length;
+              alert(map.markers[marker-1].getPosition().lat() + ' ' + map.markers[marker-1].getPosition().lng());
+            }
+          });
+          map.zoomIn(10);
+        }
+      }
+    });
+  });
+});
